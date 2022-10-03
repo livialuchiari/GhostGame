7b95342502607f8da3042d44d02aedab876cbc26
Lívia Luchiari <105221491+livialuchiari@users.noreply.github.com>
2022-10-03 15:43:12 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,6 @@ function preload(){
 function setup(){
   createCanvas(600,600);
   spookySound.loop();
-  
   tower = createSprite(300,300);
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
@@ -32,28 +31,82 @@ function setup(){
 
 function draw(){
   background(0);
- 
+  if (gameState === "play") {
+    if(keyDown("left_arrow")){
+      ghost.x = ghost.x - 3;
+    }
+    
+    if(keyDown("right_arrow")){
+      ghost.x = ghost.x + 3;
+    }
+    
+    if(keyDown("space")){
+      ghost.velocityY = -10;
+    }
+    
+    ghost.velocityY = ghost.velocityY + 0.8
+    
     if(tower.y > 400){
       tower.y = 300
     }
+    spawnDoors();
 
-
+    
+    //climbersGroup.collide(ghost);
+    if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
     if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
       ghost.destroy();
       gameState = "end"
-    }
-
-
-    if (gameState === "end"){
-      stroke("yellow");
-      fill("yellow");
-      textSize(30);
-      text("Fim de Jogo", 230,250)
     }
     
     drawSprites();
   }
   
- 
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Fim de Jogo", 230,250)
+  }
 
+}
+
+function spawnDoors() {
+  //escreva aqui o código para gerar as portas na torre
+  if (frameCount % 240 === 0) {
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    
+    door.x = Math.round(random(120,400));
+    climber.x = door.x;
+    invisibleBlock.x = door.x;
+    
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+    
+    ghost.depth = door.depth;
+    ghost.depth +=1;
+   
+    //designe tempo de vida a variável
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+
+    
+    //adicione cada porta ao grupo
+    doorsGroup.add(door);
+    invisibleBlock.debug = true;
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
+  }
+}
 
